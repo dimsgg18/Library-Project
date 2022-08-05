5b24621c836a432568bf9e576cb90d9e87dea809
DIMAS GALANG AJI WIGUNA <U547075@bca.co.id>
2022-08-05 14:44:53 +0700

    tambah box login

---
+++ src/Page/LoginPage/LoginStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const LoginBox = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items:center;
+`
+export const InLoginBox = styled.div`
+    background-color: white;
+    width : 500px;
+    height : 400px;
+    border-radius:10px;
+    margin-top: 125px;
+`
